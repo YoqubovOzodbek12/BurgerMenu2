8a9778a1b573cd57d5cd44e3a6a6b65bbafbe77f
YoqubovOzodbek12 <ozodbekyoqubob575@gmail.com>
2022-11-03 10:07:03 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 let header__btn = document.querySelector('.header__btn');
 let header__nav = document.querySelector('.header__nav')
-let overlay = document.querySelector('.overlay')
+let overflyo = document.querySelector('.overflyo')
 
 header__btn.addEventListener( 'click', function(){
     header__btn.classList.toggle('active')
     header__nav.classList.toggle('header__nav--active')
-    overlay.classList.toggle('overlay--active')
+    overflyo.classList.toggle('overflyo--active')
 });
 
-overlay.addEventListener('click' , function() {
+overflyo.addEventListener( 'click' , function() {
     header__btn.classList.toggle('active')
     header__nav.classList.toggle('header__nav--active')
-    overlay.classList.toggle('overlay--active')
+    overflyo.classList.toggle('overflyo--active')
 })
